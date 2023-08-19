d77994c110929b1bf6cb83bcfd5af1fa0cf34326
Duy Nguyen <duykhanhnguyen@kms-technology.com>
2023-08-19 10:13:20 +0700

    add Role constants

---
+++ BusinessObjects/Constants/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace BusinessObjects
+{
+    public class Roles
+    {
+        public static string ADMIN = "Admin";
+        public static string STUDENT = "Student";
+        public static string TEACHER = "Teacher";
+    }
+}
+
