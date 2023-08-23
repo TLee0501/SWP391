39d367bc2eb01f4e882d4dc61f2362c4305a48db
Duy Nguyen <duykhanhnguyen@kms-technology.com>
2023-08-23 09:44:14 +0700

    update code

--- BusinessObjects/RequestModel/CreateTaskRequest.cs
+++ BusinessObjects/RequestModel/CreateTaskRequest.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BusinessObjects.RequestModel
+﻿namespace BusinessObjects.RequestModel
 {
     public class CreateTaskRequest
     {
         public Guid ProjectId { get; set; }
         public string TaskName { get; set; } = null!;
-        public string TaskDescription { get; set; } = null!;
+        public string? TaskDescription { get; set; } = null!;
     }
 }
