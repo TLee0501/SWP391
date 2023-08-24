d3eda3364b48ebdd10cbbe7d46c8db9eefb97b66
BachTND <bachtnd@gmail.com>
2023-08-24 09:24:40 +0700

    update Class

--- Service/ClassService/IClassService.cs
+++ Service/ClassService/IClassService.cs
@@ -7,6 +7,7 @@ namespace Service.ClassService
     {
         Task<int> CreateClass(CreateClassRequest request);
         Task<ClassResponse?> GetClassByID(Guid classId, Guid? userId, string? role);
+        Task<int> UpdateClass(UpdateClassRequest request);
         Task<int> DeleteClass(Guid classId);
         Task<List<ClassResponse>> GetClasses(Guid userID, string? role, Guid? courseID, string? searchText);
         Task<bool> EnrollClass(Guid userId, Guid classId, string enrollCode);
