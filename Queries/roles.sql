3820ddb91beeb6e1b1ff7874ca6aa10cbee52986
Duy Nguyen <duykhanhnguyen@kms-technology.com>
2023-08-25 07:44:25 +0700

    add query

---
+++ Queries/roles.sql
@@ -0,0 +1,11 @@
+use SWP391OnGoingReport
+go
+
+insert into Role (roleID, roleName)
+values (newid(), 'Admin')
+
+insert into Role (roleID, roleName)
+values (newid(), 'Teacher')
+
+insert into Role (roleID, roleName)
+values (newid(), 'Student')
