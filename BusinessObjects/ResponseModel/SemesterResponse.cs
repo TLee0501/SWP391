f6e00c5e46cb858f02e621d1b93c51f0187ceaac
TLee0501 <67847896+TLee0501@users.noreply.github.com>
2023-08-28 11:49:37 +0700

    Fix semester

--- BusinessObjects/ResponseModel/SemesterResponse.cs
+++ BusinessObjects/ResponseModel/SemesterResponse.cs
@@ -4,5 +4,7 @@
     {
         public Guid SemesterId { get; set; }
         public string SemeterName { get; set; } = null!;
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
     }
 }
