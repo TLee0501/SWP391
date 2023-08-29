1ddbe6c676c1bbfa3b9603a91ff406e7dd34404a
Duy Nguyen <duykhanhnguyen@kms-technology.com>
2023-08-29 10:58:14 +0700

    update get semester detail api

---
+++ BusinessObjects/ResponseModel/SemesterDetailResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace BusinessObjects.ResponseModel
+{
+    public class SemesterDetailResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public List<ClassSemesterDetailResponse> Classes { get; set; } = null!;
+    }
+
+    public class ClassSemesterDetailResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public Guid? TeacherId { get; set; }
+        public string? TeacherName { get; set; }
+        public Guid CourseId { get; set; }
+        public string CourseName { get; set; } = null!;
+    }
+}
+
